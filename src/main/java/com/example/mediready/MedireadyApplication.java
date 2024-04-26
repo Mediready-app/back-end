28d9829b7f69611a7d607d54ac0ece51e92987e7
yulee <dbfl0461@gmail.com>
2024-04-26 21:20:53 +0900

    :tada: Spring boot 템플릿 추가 (#1)
    Spring Boot 3.2.4 버전
    Java 17
    Spring web, Lombok 의존성 추가

---
+++ src/main/java/com/example/mediready/MedireadyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedireadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedireadyApplication.class, args);
+	}
+
+}
