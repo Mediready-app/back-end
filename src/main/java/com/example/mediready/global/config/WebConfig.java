e3458ad98d872605a5febb0c1e237e1c0ad7479d
yulee <dbfl0461@gmail.com>
2024-06-10 16:31:56 +0900

    :wrench: WebConfig 설정 추가

---
+++ src/main/java/com/example/mediready/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.mediready.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8081")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
