e0a6ccc9eccf6ee58c8dfed4e9ce3b1c287cea20
yulee <dbfl0461@gmail.com>
2024-06-11 01:21:10 +0900

    :sparkles: 복용 일정 생성 전 병용금기 목록 확인 API 추가 (#22)

--- src/main/java/com/example/mediready/domain/scheduleDate/ScheduleDateController.java
+++ src/main/java/com/example/mediready/domain/scheduleDate/ScheduleDateController.java
@@ -1,10 +1,13 @@
 package com.example.mediready.domain.scheduleDate;
 
 import com.example.mediready.domain.scheduleDate.dto.CreateScheduleReq;
+import com.example.mediready.domain.scheduleDate.dto.GetScheduleDur;
 import com.example.mediready.domain.user.User;
 import com.example.mediready.global.config.BaseResponse;
 import com.example.mediready.global.config.auth.AuthUser;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,12 @@ public class ScheduleDateController {
         @RequestBody CreateScheduleReq createScheduleReq) {
         scheduleDateService.createSchedule(user, createScheduleReq);
         return new BaseResponse<>("복용 일정이 추가되었습니다.");
+    }
+
+    @GetMapping("/dur")
+    public BaseResponse<List<GetScheduleDur>> getScheduleDur(
+        @RequestBody List<Integer> medicineIdList) {
+        return new BaseResponse<>("복용 일정에 추가된 의약품간의 병용 가능 여부 정보입니다.",
+            scheduleDateService.getScheduleDur(medicineIdList));
     }
 }
--- src/main/java/com/example/mediready/domain/scheduleDate/ScheduleDateService.java
+++ src/main/java/com/example/mediready/domain/scheduleDate/ScheduleDateService.java
@@ -1,8 +1,10 @@
 package com.example.mediready.domain.scheduleDate;
 
+import com.example.mediready.domain.dur.DurRepository;
 import com.example.mediready.domain.medicine.Medicine;
 import com.example.mediready.domain.medicine.MedicineRepository;
 import com.example.mediready.domain.scheduleDate.dto.CreateScheduleReq;
+import com.example.mediready.domain.scheduleDate.dto.GetScheduleDur;
 import com.example.mediready.domain.scheduleMedicine.ScheduleMedicine;
 import com.example.mediready.domain.scheduleMedicine.ScheduleMedicineId;
 import com.example.mediready.domain.scheduleMedicine.ScheduleMedicineRepository;
@@ -10,7 +12,10 @@ import com.example.mediready.domain.user.User;
 import com.example.mediready.domain.user.UserRepository;
 import com.example.mediready.global.config.exception.BaseException;
 import com.example.mediready.global.config.exception.errorCode.MedicineErrorCode;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +28,7 @@ public class ScheduleDateService {
     private final MedicineRepository medicineRepository;
     private final ScheduleDateRepository scheduleDateRepository;
     private final ScheduleMedicineRepository scheduleMedicineRepository;
+    private final DurRepository durRepository;
 
 
     @Transactional
@@ -48,5 +54,31 @@ public class ScheduleDateService {
             ScheduleMedicine scheduleMedicine = new ScheduleMedicine(scheduleMedicineId);
             scheduleMedicineRepository.save(scheduleMedicine);
         }
+    }
+
+    public List<GetScheduleDur> getScheduleDur(List<Integer> medicineIdList) {
+        List<Medicine> medicines = medicineRepository.findAllById(medicineIdList);
+        if (medicines.size() != medicineIdList.size()) {
+            throw new BaseException(MedicineErrorCode.INVALID_MEDICINE_ID);
+        }
+
+        Map<Integer, Medicine> medicineMap = medicines.stream()
+            .collect(Collectors.toMap(Medicine::getId, medicine -> medicine));
+
+        List<GetScheduleDur> getScheduleDurs = new ArrayList<>();
+
+        for (int i = 0; i < medicineIdList.size(); i++) {
+            for (int j = i + 1; j < medicineIdList.size(); j++) {
+                Medicine medicine1 = medicineMap.get(medicineIdList.get(i));
+                Medicine medicine2 = medicineMap.get(medicineIdList.get(j));
+
+                if (durRepository.existsByDurIdPk_Id1AndDurIdPk_Id2(medicine1, medicine2)
+                    || durRepository.existsByDurIdPk_Id1AndDurIdPk_Id2(medicine2, medicine1)) {
+                    getScheduleDurs.add(
+                        new GetScheduleDur(medicine1.getName(), medicine2.getName()));
+                }
+            }
+        }
+        return getScheduleDurs;
     }
 }
