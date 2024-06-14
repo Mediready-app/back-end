1f4fd582b28a1233787a3e1965302761246ff747
yulee <dbfl0461@gmail.com>
2024-06-14 15:40:29 +0900

    :ambulance: 의약품 정보 조회시 이미지 전달

--- src/main/java/com/example/mediready/domain/medicine/MedicineService.java
+++ src/main/java/com/example/mediready/domain/medicine/MedicineService.java
@@ -72,10 +72,11 @@ public class MedicineService {
         for (Object o : jsonItemList) {
             Medicine medicine = medicineRepository.findById(id)
                 .orElseThrow(() -> new BaseException(MedicineErrorCode.INVALID_MEDICINE_ID));
+            result.setImgUrl(medicine.getImgUrl());
+
             JSONObject item = (JSONObject) o;
             result.setItemSeq(id);
             result.setItemName((String) item.get("ITEM_NAME"));
-            result.setImgUrl(medicine.getImgUrl());
             result.setEntpName((String) item.get("ENTP_NAME"));
             result.setEtcOtcCode((String) item.get("ETC_OTC_CODE"));
             result.setEeDocData(getTextFromXML((String) item.get("EE_DOC_DATA")));
